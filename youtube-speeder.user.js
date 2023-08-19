f41a71820e706318a6b9084f107e45c83f702bc0
muratcesmecioglu <muratcesmecioglu@gmail.com>
2023-08-19 16:41:36 +0300

    Update youtube-speeder.user.js
    Fix

--- youtube-speeder.user.js
+++ youtube-speeder.user.js
@@ -11,63 +11,63 @@
 // ==/UserScript==
 (function() {
     'use strict';
-
     document.addEventListener('yt-navigate-finish', () => {
         setTimeout(() => {
-            const buttonsContainer = document.getElementById("player");
-            const speedinfo = document.createElement("span");
-            speedinfo.textContent = "1.00";
-            speedinfo.id = "videoSpeedDisplay";
-            buttonsContainer.appendChild(speedinfo);
+            if (!document.getElementById("videoSpeedDisplay")) {
+                const buttonsContainer = document.getElementById("player");
+                const speedinfo = document.createElement("span");
+                speedinfo.textContent = "1.00";
+                speedinfo.id = "videoSpeedDisplay";
+                buttonsContainer.appendChild(speedinfo);
 
-            let videoSpeed = 1.0;
-            const videoSpeedDisplay = document.getElementById("videoSpeedDisplay");
+                let videoSpeed = 1.0;
+                const videoSpeedDisplay = document.getElementById("videoSpeedDisplay");
 
 
-            function changeSpeedWithAnimation(targetSpeed) {
-                const initialSpeed = videoSpeed;
-                const duration = 1000;
-                const steps = 60;
+                function changeSpeedWithAnimation(targetSpeed) {
+                    const initialSpeed = videoSpeed;
+                    const duration = 1000;
+                    const steps = 60;
 
-                function easeOutQuad(t) {
-                    return t * (2 - t);
-                }
-
-                function animate(time) {
-                    const elapsedTime = time - startTime;
-                    const progress = Math.min(elapsedTime / duration, 1);
-                    const easedProgress = easeOutQuad(progress);
-                    videoSpeed = initialSpeed + (targetSpeed - initialSpeed) * easedProgress;
-                    document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = videoSpeed.toFixed(2);
-                    videoSpeedDisplay.textContent = videoSpeed.toFixed(2);
-
-                    if (progress < 1) {
-                        requestAnimationFrame(animate);
+                    function easeOutQuad(t) {
+                        return t * (2 - t);
                     }
+
+                    function animate(time) {
+                        const elapsedTime = time - startTime;
+                        const progress = Math.min(elapsedTime / duration, 1);
+                        const easedProgress = easeOutQuad(progress);
+                        videoSpeed = initialSpeed + (targetSpeed - initialSpeed) * easedProgress;
+                        document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = videoSpeed.toFixed(2);
+                        videoSpeedDisplay.textContent = videoSpeed.toFixed(2);
+
+                        if (progress < 1) {
+                            requestAnimationFrame(animate);
+                        }
+                    }
+
+                    const startTime = performance.now();
+                    animate(startTime);
                 }
 
-                const startTime = performance.now();
-                animate(startTime);
+                function createButton(text, speed) {
+                    const button = document.createElement("button");
+                    button.textContent = text;
+                    button.addEventListener("mousedown", function() {
+                        changeSpeedWithAnimation(speed);
+                    });
+                    button.addEventListener("mouseup", function() {
+                        changeSpeedWithAnimation(1);
+                    });
+                    buttonsContainer.appendChild(button);
+                }
+
+                createButton("1.5x", 1.5);
+                createButton("2x", 2);
+                createButton("5x", 5);
+                createButton("10x", 10);
+                createButton("Reset", 1);
             }
-
-            function createButton(text, speed) {
-                const button = document.createElement("button");
-                button.textContent = text;
-                button.addEventListener("mousedown", function() {
-                    changeSpeedWithAnimation(speed);
-                });
-                button.addEventListener("mouseup", function() {
-                    changeSpeedWithAnimation(1);
-                });
-                buttonsContainer.appendChild(button);
-            }
-
-            createButton("1.5x", 1.5);
-            createButton("2x", 2);
-            createButton("5x", 5);
-            createButton("10x", 10);
-            createButton("Reset", 1);
-
         }, 2000);
     });
 
